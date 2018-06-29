3f8d90fa63fa3dbd1fd5b8d4084c3c2fc02d8f0e
mizchi <miz404@gmail.com>
2018-06-29 13:08:57 +0900

    Init as redux project

--- components/App.js
+++ components/App.js
@@ -1,2 +1,41 @@
+// @flow
 import React from "react";
-export default () => <span>This is App component</span>;
+import ReactRedux from "react-redux";
+import Redux from "redux";
+
+const { Provider, connect } = ReactRedux;
+
+type State = {
+  value: number
+};
+
+const initialState: State = {
+  value: 0
+};
+
+const counter = (state: State = initialState, action: any) => {
+  switch (action.type) {
+    case "add": {
+      return { value: state.value + 1 };
+    }
+  }
+  return state;
+};
+
+const rootReducer = Redux.combineReducers({ counter });
+
+const App = connect(state => state.counter)(props => {
+  return (
+    <div>
+      value: {props.value}
+      <button onClick={() => props.dispatch({ type: "add" })}>+1</button>
+    </div>
+  );
+});
+
+const store = Redux.createStore(rootReducer);
+export default () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
